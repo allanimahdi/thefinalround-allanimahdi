d975500e1c4b85ce26004aee70cd8047564a1ac6
allanimahdi <allanimohamedmahdi@gmail.com>
2017-01-10 10:28:11 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/correction_exam');
+mongoose.connect('mongodb://allani:192591@ds159988.mlab.com:59988/correction_exam');
 module.exports = mongoose;
